5b001a432c6221ed05bdab91be6411389f33ad0a
Ayush Anand <ayushanand109@gmail.com>
2026-08-13 18:53:37 +0530

    Initial commit: BlogApp tutorial till lec21 appwrite services

---
+++ src/appwrite/auth.js
@@ -0,0 +1,69 @@
+import conf from '../conf/conf.js'
+import {Client,Account, ID} from "appwrite";
+
+class AuthService{
+    client =new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account=new Account(this.client);
+
+    }
+//yaha new method create kr rhe custom methods taki vendor lockin se bach paye
+    async createAccount({email,Password,name}){
+        try{
+            const userAccount=await this.account.create(ID.unique(),email,Password,name);
+            if(userAccount){
+                //call another method
+                return this.login({email,password});
+                
+            }
+            else{
+                //ho skta hai null value ayi ho to us hisab se handlekr lenge
+                return userAccount;
+            }
+            
+
+        }
+        catch(error){
+            throw error;
+        }
+    }
+
+    async login({email,password}){
+        try{
+            return await this.account.createEmailSession(email,password);
+        }
+        catch(error){
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        }
+        catch(error){
+            console.log("Appwrite service:: getCurrentUser::error",error);
+        }
+        //agr kisi wajah se try catch me error aata hai to null return kr denge taki app me handle kr sake
+        return null;
+
+    }
+    async logout(){
+        try{
+            return await this.account.deleteSessions('current');
+        }
+        catch(error){
+            console.log("Appwrite service:: logout::error",error);
+        }
+    }
+
+}
+
+const authService=new AuthService();
+
+export default authService;
