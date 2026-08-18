5d7f44f1260c0f3c50c061c868a9c5712203bb26
Ayush Anand <ayushanand109@gmail.com>
2026-08-18 18:07:57 +0530

    configured redux & created some production grade components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import {useDispatch} from 'react-redux';
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+// import {outlet} from "react-router-dom";
 
 import './App.css'
 
 function App() {
-  
+  const [loading,setLoading]=useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-    <h1> A blog with Appwrite</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(!userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+      
+    })
+    .finally(()=>setLoading(false))
+  },[]);
+
+
+//conditional rendering krenge ki agr loading true hai toh loading displayk krenge
+// nahi to kuch or
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-centre bg-gray-400'>
+      <div className='w-full block'>
+             <Header/> 
+             <main>
+              
+             </main>
+             <Footer/>
+      </div>
+
+    </div>
+  ):null
 }
 
 export default App
---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {useDispatch } from 'react-redux';
+import authService from "../../appwrite/auth"
+import {logout} from "../../store/authSlice"
+
+function LogoutBtn() {
+
+    const dispatch=useDispatch();
+    const logoutHandler=()=>{
+        //logur apne aap me ek promise hai
+        authService.logout().then(()=>{
+            dispatch(logout())
+        })
+    }
+  return (
+    <button className='inline-block px-6 py-2.5 duration-200 hover:bg-red-600 rounded-full' onClick={logoutHandler}>Logout</button>
+  )
+}
+
+export default LogoutBtn
